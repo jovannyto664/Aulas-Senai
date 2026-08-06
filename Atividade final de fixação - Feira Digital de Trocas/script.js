32da547745881b1cd7cf92a169bb6e6e65938488
Jovannyto664 <jovannyto664@gmail.com>
2026-08-06 11:17:11 -0300

    Adicionar funcionalidade de formulário e elementos de interface

--- Atividade final de fixação - Feira Digital de Trocas/script.js
+++ Atividade final de fixação - Feira Digital de Trocas/script.js
@@ -1,3 +1,11 @@
 const formulario = document.getElementById("formItem")
 const pesquisa = document.getElementById("pesquisa")
 const filtroCat = document.getElementById("filtroCategoria")
+const filtroTipo = document.getElementById("filtroTipo")
+const filtroSituacao = document.querySelector("#filtroSituacao")
+const mensagem = document.getElementById("mensagem")
+const listaItens = document.getElementById("listaItens")
+formulario.addEventListener("submit", (event) =>{
+    event.preventDefault()
+})
+
